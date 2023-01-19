fd0f3ab9c2cc86f27a08d9d8dc7d1e4c3d7c35f0
Kiran1695 <hindlekarkiran.kh@gmail.com>
2023-01-19 14:07:41 +0530

    Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...

--- src/com/bridgelabz/AddressBookSystem.java
+++ src/com/bridgelabz/AddressBookSystem.java
@@ -1,7 +1,50 @@
 package com.bridgelabz;
 
+import javax.swing.*;
+import java.util.Scanner;
+
 public class AddressBookSystem {
+
+    public static void address() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Your First Name : ");
+        String first_name = scanner.next();
+
+        System.out.println("Enter Your Last Name : ");
+        String last_name = scanner.next();
+
+        System.out.println("Enter Your Address : ");
+        String address = scanner.next();
+
+        System.out.println("Enter Your City : ");
+        String city = scanner.next();
+
+        System.out.println("Enter Your State : ");
+        String state = scanner.next();
+
+        System.out.println("Enter Your Zip Code : ");
+        String zip_code = scanner.next();
+
+        System.out.println("Enter Your Phone Number : ");
+        String phone_number = scanner.next();
+
+        System.out.println("Enter Your Email Id : ");
+        String email_id = scanner.next();
+
+        System.out.println("First Name : " + first_name);
+        System.out.println("Last Name : " + last_name);
+        System.out.println("Address : " + address);
+        System.out.println("City : " + city);
+        System.out.println("State : " + state);
+        System.out.println("Zip Code : " + zip_code);
+        System.out.println("Phone Number : " + phone_number);
+        System.out.println("Email Id : " + email_id);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome To Address Book Program.");
+        System.out.println("Welcome To Address Book Program");
+        address();
     }
 }
